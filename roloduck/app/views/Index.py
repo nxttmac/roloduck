5e53a3ddc90630cfd0c7b1b5f507669498474e1e
Peter <pemjohns@gmail.com>
2014-02-28 23:58:17 -0600

    Hashed Passwords

--- roloduck/app/views/Index.py
+++ roloduck/app/views/Index.py
@@ -8,76 +8,88 @@ from flask import render_template, request, redirect, session, url_for, flash
 from app import app
 from app.db import db
 from app.db.UserDao import UserDao
+from app.db.PartnerDao import PartnerDao
+from app.db.ProjectDao import ProjectDao
 from app.models.User import User
 from app.models.User import COMPANY_TYPE_FREE, ROLE_ADMIN
 from flask.ext.login import login_required, login_user
 
-
 # Create our UserDao to connect to the user collection
 user_dao = UserDao(db)
+project_dao = ProjectDao(db)
+partner_dao = PartnerDao(db)
 
-# This will handle routes to our splash screen
+
 @app.route("/index", methods=['GET'])
 @app.route("/index.html", methods=['GET'])
 @app.route("/", methods=['GET'])
 def roloduck_index():
-    # No exception thrown for lists
-    userlist = user_dao.find_all()
+    """
+    Route users who are not logged in to the index.
+    If they are logged in, send them to the projects index
+    """
     try:
-        if session['username'] is not None:
+        if session['logged_in'] is not None:
             return redirect('/projects')
         else:
-            return render_template('index.html', userlist=userlist)
+            return render_template('index.html')
     except KeyError:
-        return render_template('index.html', userlist=userlist)
+        return render_template('index.html')
 
-# Welcome page after a user signs in
-@app.route("/welcome", methods=['GET'])
-@login_required
-def welcome_page():
-    user = user_dao.find_user_by_hash(session['username'])
-    return render_template('welcome.html', user=user)
 
-# Serve the signup form
 @app.route("/signup", methods=['GET'])
 def serve_signup_form():
+    """
+    Serve the signup form to the user
+    """
     return render_template('signup.html')
 
-# For now this is attached to posting the form in the navbar, but its a basic
-# mechanism to insert a user based off a form
+
 @app.route("/signup", methods=['POST'])
 def post_signup_form():
-    # Retrive the information from the form
+    """
+    Post the signup form, register the user, and sign them in
+    """
+    # Retrieve the information from the form
     name = request.form.get('userName')
     email = request.form.get('userEmail')
-    password = request.form.get('userPassword')
-    companyName = request.form.get('companyName')
+    # Hash the password
+    password = User.hide_user_password(request.form.get('userPassword'))
+    company_name = request.form.get('companyName')
     # TODO hardcoded sub type
-    companySubscriptionType = COMPANY_TYPE_FREE
+    company_subscription_type = COMPANY_TYPE_FREE
     # Create the company
-    company = {'companyName': companyName, 'companySubscriptionType': companySubscriptionType}
+    company = {'companyName': company_name, 'companySubscriptionType': company_subscription_type}
     # TODO hardcoded role to admin
     role = ROLE_ADMIN
     new_user = User(name, email, password, role, company)
     if new_user is not None:
         user_dao.insert_obj(new_user.get_user_map())
+        hash = new_user.get_login_hash()
+        session['logged_in'] = hash
         login_user(new_user)
-        session['username'] = email+password
         flash(u'Welcome to Roloduck!', 'success')
         return redirect('/')
 
-# A page only to be shown for debugging purposes
-# Will be updated to show current database content/state
-@app.route("/db", methods=['GET'])
-def serve_database_info():
-    userlist = user_dao.find_users()
-    return render_template('database-view.html', userlist=userlist)
 
-# Serve the page to add a new user to your team/client/company
 @app.route("/adduser", methods=['GET'])
 def serve_create_company_user():
-    user = user_dao.find_user_by_hash(session['username'])
+    """
+    Serve the page to add a new user to your team/client/company
+    """
+    try:
+        user_hash = session['logged_in']
+    except KeyError:
+        return redirect(url_for('login_page.html'))
+    user = user_dao.find_user_by_hash(user_hash)
     if user is not None:
-        if user['role']== 1:
+        if user['role'] == 1:
             return render_template('create-user.html', currentUser=user)
     return redirect(url_for('login_page.html'))
+
+@app.route("/wipe")
+def wipe_db():
+    user_dao.remove_all()
+    partner_dao.remove_all()
+    project_dao.remove_all()
+    return redirect('/')
--- roloduck/app/views/Login.py
+++ roloduck/app/views/Login.py
@@ -7,43 +7,51 @@ from flask.ext.login import logout_user, login_user, login_required
 from app import app, login_manager
 from app.db import db
 from app.db.UserDao import UserDao
-#import hashlib
+from app.models.User import User
 
 # Create our user_dao
 user_dao = UserDao(db)
+
 
 @app.route("/login", methods=['GET'])
 def login_page_get():
     return render_template('login-page.html')
 
+
 @app.route("/login", methods=['POST'])
 def login_page_post():
     email = request.form['useremail']
-    password = request.form['userpassword']
-    hash_formula = email + password
-    user = user_dao.find_user_by_hash(hash_formula)
+    password = User.hide_user_password(request.form['userpassword'])
+    user = user_dao.find_user_by_email(email)
     # Login was not successful
-    if user is None:
+    if user is None or user['password'] != password:
         flash(u'Invalid login!  Please try again.', 'danger')
         return redirect('/login')
     # Login was successful so officially log the user is and add their hash to the session
-    login_user(user)
-    #sha = hashlib.sha1(email + password)
-    session['username'] = hash_formula
-    flash(u'Welcome back, %s.' % user.name, 'success')
+    session['logged_in'] = user['login_hash']
+    # TODO make sure this is safe, we dont want to create a diff User object
+    # and log it in instead of the original user
+    user_obj = User(user['name'], user['email'], user['password'],
+                    user['role'], user['company'], user['login_hash'])
+    login_user(user_obj)
+    flash(u'Welcome back, %s.' % user_obj.name, 'success')
     return redirect('/')
+
 
 # Create user loader function
 @login_manager.user_loader
 def load_user(userid):
-    if session['username'] is not None:
-        user = user_dao.find_user_by_hash(session['username'])
+    if session['logged_in'] is not None:
+        user = user_dao.find_user_by_hash(session['logged_in'])
         if user is not None:
-            return user
+            user_obj = User(user['name'], user['email'], user['password'],
+                            user['role'], user['company'], user['login_hash'])
+            return user_obj
+
 
 @app.route("/logout")
 #@login_required
 def logout():
     logout_user()
-    session.pop('username', None)
+    session.pop('logged_in', None)
     return redirect('/')
